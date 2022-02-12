9195165b89f17cb858dfe06ccaff48caba508523
24by7event <96644481+24by7event@users.noreply.github.com>
2022-02-12 16:08:57 +0530

    Project set up

---
+++ 24by7Event Live-SourceApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  24by7Event Live-SourceApp
+//
+//  Created by Vivek on 12/02/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target '24by7Event Live-SourceApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+    pod 'HaishinKit', '~> 1.2.2'
+
+  # Pods for 24by7Event Live-SourceApp
+
+end
+
+
+com.Example.24by7Event
